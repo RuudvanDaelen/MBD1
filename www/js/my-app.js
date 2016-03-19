9617ba08dec608f5751863905f419698f043783d
RuudvanDaelen <rdaelen@student.avans.nl>
2016-03-19 19:01:16 +0100

    Pokemons worden ingeladen, left pane kan geopend worden (leeg), detail pagina (leeg) kan geopdend worden.

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -47,3 +47,43 @@ function createContentPage() {
     );
 	return;
 }
+
+
+//Wordt direct uitgevoerd wanneer my-app.js wordt geladen in de pagina
+$.ajax({url: "http://pokeapi.co/api/v2/pokemon/", success: function(result){
+        //alert("Aantal pokemons: " + result.count + "\n" + result.next);
+        $.each(result.results, function(i, item) {
+            //var pokemonItem = { name : item.name, href: item.url }
+            
+            //Id
+            var regMatch = item.url.match(/\d+\/$/);
+            var pokemonId = regMatch[0].slice(0, -1);
+            //Image
+            var imageUrl = "http://pokeapi.co/media/img/" + pokemonId + ".png";
+            //Name (eerste letter uppercase maken)
+            var lowerName = item.name;
+            var pokemonName = lowerName.toLowerCase().replace(/\b[a-z]/g, function(letter) {
+               return letter.toUpperCase(); 
+            });
+            
+            
+            
+            var pokemon = { id: pokemonId, name: pokemonName, href: item.url, image: imageUrl }
+            //alert(pokemon.id + ": " + pokemon.name + ": " + pokemon.href + ": " + pokemon.image);
+            
+            var html = "<li>";
+                html += "<a href='detail.html' class='item-link item-content'>";
+                    html += "<div class='item-media'>";
+                        html += "<img height='88px' width='88px' src='"+ pokemon.image +"'>";
+                    html += "</div>";
+                    html += "<div class='item-inner'>";
+                        html += "<div class='item-title-row'>";
+                            html += "<div class='item-title'>"+ pokemon.name +"</div>";
+                        html += "</div>";
+                        html += "<div class='item-subtitle'># "+ pokemon.id +"</div>";
+                    html += "</div>";
+                html += "</a>";
+            html += "</li>"; 
+            $("#pokemon-list ul").append(html);
+        });
+}});
